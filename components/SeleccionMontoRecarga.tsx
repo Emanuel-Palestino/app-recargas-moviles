16776d17d1c53139b5a65e326e1a5c6fb84d3cd3
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-03-17 14:57:58 -0600

    [recargar] Botón para ver beneficios de la recarga agregado.

--- components/SeleccionMontoRecarga.tsx
+++ components/SeleccionMontoRecarga.tsx
@@ -1,6 +1,6 @@
 import { Picker } from "@react-native-picker/picker"
 import { FC } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native"
 
 
 interface SeleccionMontoRecargaProps {
@@ -16,22 +16,31 @@ export const SeleccionMontoRecarga: FC<SeleccionMontoRecargaProps> = ({ setMonto
 				Monto de Recarga
 			</Text>
 
-			<View style={styles.picker} >
-				<Picker
-					selectedValue={monto}
-					onValueChange={setMonto}
-				>
-					<Picker.Item label="$10" value="10" />
-					<Picker.Item label="$20" value="20" />
-					<Picker.Item label="$30" value="30" />
-					<Picker.Item label="$50" value="50" />
-					<Picker.Item label="$100" value="100" />
-					<Picker.Item label="$150" value="150" />
-					<Picker.Item label="$200" value="200" />
-					<Picker.Item label="$300" value="300" />
-					<Picker.Item label="$500" value="500" />
-				</Picker>
+			<View style={styles.contenedorBeneficios}>
+				<View style={styles.picker} >
+					<Picker
+						selectedValue={monto}
+						onValueChange={setMonto}
+					>
+						<Picker.Item label="$10" value="10" />
+						<Picker.Item label="$20" value="20" />
+						<Picker.Item label="$30" value="30" />
+						<Picker.Item label="$50" value="50" />
+						<Picker.Item label="$100" value="100" />
+						<Picker.Item label="$150" value="150" />
+						<Picker.Item label="$200" value="200" />
+						<Picker.Item label="$300" value="300" />
+						<Picker.Item label="$500" value="500" />
+					</Picker>
+				</View>
+
+				<Pressable style={styles.botonBeneficios} onPress={() => Alert.alert('Beneficios', 'beneficios')}>
+					<Text style={styles.textoBotonBeneficios}>
+						Beneficios
+					</Text>
+				</Pressable>
 			</View>
+
 		</View >
 	)
 
@@ -46,10 +55,25 @@ const styles = StyleSheet.create({
 		color: 'gray'
 	},
 	picker: {
-		width: '100%',
+		width: '50%',
 		height: 54,
 		padding: 0,
 		borderRadius: 10,
-		backgroundColor: '#eeeae8'
+		backgroundColor: '#eeeae8',
+		flexGrow: 1
+	},
+	contenedorBeneficios: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 10
+	},
+	botonBeneficios: {
+		backgroundColor: 'lightblue',
+		paddingHorizontal: 16,
+		paddingVertical: 10,
+		borderRadius: 10,
+	},
+	textoBotonBeneficios: {
+		color: '#000'
 	}
 })
